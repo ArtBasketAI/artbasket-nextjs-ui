d370aefab7aaeeb81b2d868a42b941b5ff317a00
bharath <bharathts07@gmail.com>
2024-03-27 19:30:24 -0400

    add docs link to navbar item list

--- components/DefaultNavLinks.tsx
+++ components/DefaultNavLinks.tsx
@@ -17,6 +17,11 @@ const DefaultNavLinks = () => {
             <Link href="/contact" passHref>
                 <span className="text-gray-800 font-bold cursor-pointer">Contact</span>
             </Link>
+            <Link href="https://docs.artbasket.ai/" legacyBehavior>
+                <a target="_blank" rel="noopener noreferrer" className="text-gray-800 font-bold cursor-pointer">
+                    Docs
+                </a>
+            </Link>
         </>
     );
 };
